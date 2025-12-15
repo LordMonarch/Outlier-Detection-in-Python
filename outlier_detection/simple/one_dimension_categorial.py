c9c21c72075dd2d8636a415f74f4391b36ef2027
LordMonarch <psnrainer@gmail.com>
2025-12-15 08:24:06 +0100

    Zwischenstand

--- outlier_detection/simple/one_dimension_categorial.py
+++ outlier_detection/simple/one_dimension_categorial.py
@@ -13,6 +13,7 @@ class OneDimCategorial(Detection):
         super().__init__(data)
         self.data[NAME_DATA] = self.data[NAME_DATA].astype("category")
         self.threshold = threshold
+        self.counter = None
 
     def calculate(self) -> None:
         pdf = self.data
@@ -22,11 +23,16 @@ class OneDimCategorial(Detection):
         vc = vc.sort_values()
         # Gets the cumulative count of each unique value
         cumm_frac = np.cumsum(vc.values) / len(pdf)
+        print(cumm_frac)
+        print(vc.values)
         # Finds the values with low cumulative counts
         num_rare_vals = np.where(cumm_frac < self.threshold)[0].max()
         cut_off = vc.values[::-1][num_rare_vals]
-        min_count = vc[cut_off]
-        print(cut_off)
+        #min_count = vc[cut_off]
+        print(num_rare_vals)
+        #logger.success(
+        #    f"IQR Outliers gefunden threshold={self.threshold}: {len(self.outliers)}/{len(self.data)}"
+        #)
 
         fix, ax = plt.subplots(figsize=(10, 2))
         s = sns.barplot(x=vc.index, y=vc.values, order=vc.index, color="blue")
@@ -44,6 +50,6 @@ if __name__ == "__main__":
     path = Path(f.__file__).parent.joinpath("SpeedDating.csv").absolute()
     print(path)
     d = pd.read_csv(path, header=0)
-    odc = OneDimCategorial(d["cat1"].to_list())
+    odc = OneDimCategorial(d["age"].to_list())
     odc.calculate()
     print(d)
